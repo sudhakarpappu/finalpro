10b511e0d4e7f683a9520c5992c83cb44ed5d09d
sudhakarpappu <62554341+sudhakarpappu@users.noreply.github.com>
2025-09-06 12:13:13 +0530

    day-6

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ from controllers import routes
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render sets PORT automatically
+    port = int(os.environ.get("PORT", 8000))  # Render sets PORT automatically
     app.run(host="0.0.0.0", port=port)
   # Run the app on port 5000
